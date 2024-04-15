60b8e75cdff2a6da3b86d423c3ba14b9390d2dfe
DaniilBehus <daniil.behus@gmail.com>
2024-04-15 17:11:27 +0200

    Kod spline, koniec SVG a miesto komentarov "//"

--- z1.DaniilBehus.cpp
+++ z1.DaniilBehus.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
+//
 char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
     if (n < 2) {
         printf("Chyba: Musite zadat aspon dva body zlomu.\n");
         return 0;
-        
+        //
         fprintf(file, "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 100 100\">\n");
         fprintf(file, "<line x1=\"0\" y1=\"50\" x2=\"100\" y2=\"50\" stroke=\"black\"/>\n");
         fprintf(file, "<line x1=\"50\" y1=\"0\" x2=\"50\" y2=\"100\" stroke=\"black\"/>\n");
+        //
+        fprintf(file, "<polyline points=\"");
+    for (int i = 0; i < n; i++) {
+        fprintf(file, "%.2f,%.2f ", x[i], 100 - y[i]); 
     }
+    fprintf(file, "\" fill=\"none\" stroke=\"red\"/>\n");
+    fprintf(file, "</svg>");
+    return 1;
 	}
