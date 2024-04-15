1eb658e228a632665538cc20650616e1f3e2ac90
DaniilBehus <daniil.behus@gmail.com>
2024-04-15 16:22:43 +0200

    Funkcia SVG a  pocet boduv >1

--- z1.DaniilBehus.cpp
+++ z1.DaniilBehus.cpp
@@ -1,1 +1,8 @@
 #include <stdio.h>
+
+char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
+    if (n < 2) {
+        printf("Chyba: Musite zadat aspon dva body zlomu.\n");
+        return 0;
+    }
+	}
