bf1614ec284929df95b0bef1a9580b761a112986
DaniilBehus <daniil.behus@gmail.com>
2024-04-15 18:26:24 +0200

    Chybu somopravil pomocou"{" a dostal som graf

--- z1.DaniilBehus.cpp
+++ z1.DaniilBehus.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-//
 char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
     if (n < 2) {
         printf("Chyba: Musite zadat aspon dva body zlomu.\n");
         return 0;
+    }
         //
         fprintf(file, "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 100 100\">\n");
         fprintf(file, "<line x1=\"0\" y1=\"50\" x2=\"100\" y2=\"50\" stroke=\"black\"/>\n");
@@ -18,8 +18,8 @@ char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
     return 1;
 	}
     
-    //
-int main() {
+   
+  int main(){
     FILE *file = fopen("graph.svg", "w");
     if (file == NULL) {
         printf("Nepodarilo sa otvorit subor na zapis.\n");
