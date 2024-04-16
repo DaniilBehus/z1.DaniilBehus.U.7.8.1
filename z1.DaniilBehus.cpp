91488e2140f201c879f69d463ca897d21b4e45d3
DaniilBehus <daniil.behus@gmail.com>
2024-04-16 16:12:15 +0200

    Teraz su body zapletky nahodne

--- z1.DaniilBehus.cpp
+++ z1.DaniilBehus.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
+
 //1
 char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
     if (n < 2) {
@@ -6,11 +9,11 @@ char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
         return 0;
     }
         //2
-        fprintf(file, "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 100 100\">\n");
-        fprintf(file, "<line x1=\"0\" y1=\"50\" x2=\"100\" y2=\"50\" stroke=\"black\"/>\n");
-        fprintf(file, "<line x1=\"50\" y1=\"0\" x2=\"50\" y2=\"100\" stroke=\"black\"/>\n");
+    fprintf(file, "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 100 100\">\n");
+    fprintf(file, "<line x1=\"0\" y1=\"50\" x2=\"100\" y2=\"50\" stroke=\"black\"/>\n");
+    fprintf(file, "<line x1=\"50\" y1=\"0\" x2=\"50\" y2=\"100\" stroke=\"black\"/>\n");
         //3
-        fprintf(file, "<polyline points=\"");
+    fprintf(file, "<polyline points=\"");
     for (int i = 0; i < n; i++) {
         fprintf(file, "%.2f,%.2f ", x[i], 100 - y[i]); 
     }
@@ -27,9 +30,14 @@ char SVG_KRESLI_SPLINE(FILE *file, float x[], float y[], int n) {
         return 1;
     }
     //5
-    float x[] = {10, 30, 50, 70, 90};
-    float y[] = {20, 40, 60, 80, 50};
-    int n = sizeof(x) / sizeof(x[0]);  
+    srand(time(0)); 
+    int n = 5; 
+    float x[n];
+    float y[n];
+    for (int i = 0; i < n; i++) {
+        x[i] = (float)rand()/(float)(RAND_MAX/100); 
+        y[i] = (float)rand()/(float)(RAND_MAX/100); 
+    }
     //6
 		char result = SVG_KRESLI_SPLINE(file, x, y, n);
     if (!result) {
